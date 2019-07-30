19183e9e46506cdf9e815c25fad36243172d6af9
onassis osunwa <onxssis@gmail.com>
2019-07-30 23:31:29 +0100

    bad merge

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,15 +12,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-<<<<<<< HEAD
-        //
-=======
-        // if (env('APP_ENV') === 'production') {
-        //     \URL::forceScheme('https');
-        // }
->>>>>>> c13cc08ea92831c433849b4ffd51d54ca775b321
-    }
+    { }
 
     /**
      * Register any application services.
