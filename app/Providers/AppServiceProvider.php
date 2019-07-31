fbc38144a4a3691a77c37dd4c93a6fff8906cf94
onassis osunwa <onxssis@gmail.com>
2019-07-31 03:35:41 +0100

    https redirect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    { }
+    {
+        if (env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+        }
+    }
 
     /**
      * Register any application services.
