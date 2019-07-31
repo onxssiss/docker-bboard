f1cdb0fb9238d0427ccb7e5b3f4c254e9cde3847
onassis osunwa <onxssis@gmail.com>
2019-07-31 13:00:43 +0100

    revert k8s configs

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
-# docker build -t onxssis/birdboard-app-k8s:latest -t onxssis/birdboard-app-k8s:$SHA -f "Dockerfile" .
+docker build -t onxssis/birdboard-app-k8s:latest -t onxssis/birdboard-app-k8s:$SHA -f "Dockerfile" .
 
-# docker push onxssis/birdboard-app-k8s:latest
+docker push onxssis/birdboard-app-k8s:latest
 
-# docker push onxssis/birdboard-app-k8s:$SHA
+docker push onxssis/birdboard-app-k8s:$SHA
 
-# kubectl apply -f k8s
-# kubectl set image deployments/birdboard-deployment birdboard=onxssis/birdboard-app-k8s:$SHA
-
-kubectl delete -f k8s
-kubectl apply -f k8s2
+kubectl apply -f k8s
+kubectl set image deployments/birdboard-deployment birdboard=onxssis/birdboard-app-k8s:$SHA
