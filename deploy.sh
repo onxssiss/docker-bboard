d4a65a216172cf4ea9a57ca1b64f82e8c76ec224
onassis osunwa <onxssis@gmail.com>
2019-07-31 02:37:16 +0100

    add gke deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+docker build -t onxssis/birdboard-app-k8s:latest -t onxssis/birdboard-app-k8s:$SHA -f "Dockerfile" .
+
+docker push onxssis/birdboard-app-k8s:latest
+
+docker push onxssis/birdboard-app-k8s:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/birdboard-deployment birdboard=onxssis/birdboard-app-k8s:$SHA
