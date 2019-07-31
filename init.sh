fd7ebdb353258f0bf403ae82cf4d295531792e70
onassis osunwa <onxssis@gmail.com>
2019-07-31 15:20:43 +0100

    change mysql connect string

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 echo 'Waiting for Mysql to be available'
 maxTries=10
-while [ ${maxTries} -gt 0 ] && ! mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} -e exit; do
+while [ ${maxTries} -gt 0 ] && ! mysql --user=${DB_USERNAME} --password=${DB_PASSWORD} --host=${DB_HOST} --port=${DB_PORT} --database=${DB_DATABASE} -e exit; do
     sleep 15
 done
 
